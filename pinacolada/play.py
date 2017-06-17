f96d68772457e99b58db3be6a90f71e7e8371e3b
Lars Olsson <l.a.olsson@gmail.com>
2017-06-17 19:35:32 +1000

    random stuff

--- pinacolada/play.py
+++ pinacolada/play.py
@@ -1,63 +1,36 @@
 import os
-#import numpy as np
-import cv2
-
 import sys
+import time
+
+import cv2
 import numpy as np
 
-sleep = float(sys.argv[3])
+import processing
+import utils
 
-path = sys.argv[2]
-list_names = []
 
-for p, d, files in os.walk(path):
-    for f in files:
-        if f.endswith('.png') and f.startswith(sys.argv[1]):
-            list_names.append(f)
+def main(argv):
+    image_prefix = argv[0]
+    from_path = argv[1]
+    sleep = float(argv[2])
 
-list_names.sort()
+    images_to_play = utils.get_images_in_dir(from_path, image_prefix)
 
-count = 0
-first=True
-prev = None
-while(1):
-    print(count, len(list_names))
-    print(list_names[count])
+    images_to_play.sort()
 
-    img = cv2.imread(os.path.join(path, list_names[count]))
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(img, list_names[count],(10,20), font, 0.7,(255,255,255),1)# ,cv2.LINE_AA)
-    count += 1
-    print(img.shape)
-
-    #if first:
-    #    screen_res = 1280, 720
-    #    scale_width = screen_res[0] / img.shape[1]
-    #    scale_height = screen_res[1] / img.shape[0]
-    #    scale = min(scale_width, scale_height)
-    #    window_width = int(img.shape[1] * scale)
-    #    window_height = int(img.shape[0] * scale)
+    count = 1
+    for i in images_to_play:
+        print('Playing {} {}/{} images'.format(i, count, len(images_to_play)))
+        im = cv2.imread(os.path.join(from_path, i))
+        cv2.imshow('Play'.format(from_path), im)
         
-    #    cv2.namedWindow('Play {}'.format(sys.argv[1]), cv2.WINDOW_NORMAL)
-    #    cv2.resizeWindow('Play {}'.format(sys.argv[1]), window_width, window_height)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
         
-    cv2.imshow('Play {}'.format(sys.argv[1]), img)
+        count += 1
+        time.sleep(sleep)
+        
 
-#    if prev is not None:
-#        print('ff')
-#        cv2.imshow('Prev', cv2.subtract(prev, img))
-#    prev = img
-
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
-
-    import time
-    time.sleep(sleep)
-
-    if count == len(list_names):
-        count = 0
-
-
-cv2.destroyAllWindows()
-cap.release()
+if __name__ == '__main__':
+    main(sys.argv[1:])
