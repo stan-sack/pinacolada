615b1b8cde5378b00b8113873d198d098c057c6f
Stan Tsouvallas <stsou@stsou-mbp.dolby.net>
2017-10-08 12:52:00 +1100

    implement bounds

--- frontend/src/routes/Home/components/SydneyMap/SydneyMap.js
+++ frontend/src/routes/Home/components/SydneyMap/SydneyMap.js
@@ -3,10 +3,11 @@ import React from 'react'
 import './SydneyMap.scss'
 import { Map, TileLayer } from 'react-leaflet'
 
-const position = [-33.7, 151.21]
+// const position = [-33.7, 151.21]
+const bounds = [[-34.28, 150.50], [-33.13, 151.9]]
 const SydneyMap = props => (
 	<div>
-		<Map center={position} zoom={5}>
+		<Map bound={bounds}>
 			<TileLayer
 				url='https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}@2x.png' />
 		</Map>
